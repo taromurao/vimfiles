cb5cac61f494f14b9c12ab4f60732d40a034da4b
Taro Murao <taro.murao@gmail.com>
2014-08-02 00:55:25 +0200

    deactivate vimproc

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 cp -r ~/.vim ~/.vim.old 2>/dev/null
 rm -fr ~/.vim 2>/dev/null
 rm -f ~/.vimrc 2>/dev/null
-git clone https://github.com/taromurao/vimfiles ~/.vim && cd ~/.vim && vim -c BundleInstall! -c q -c q -u bundles.vim && ln -s ~/.vim/vimrc ~/.vimrc && git clone git@github.com:Shougo/vimproc.vim.git ~/.vim/bundle/vimproc.vim && cd ~/.vim/bundle/vimproc.vim && make
+git clone https://github.com/taromurao/vimfiles ~/.vim && cd ~/.vim && vim -c BundleInstall! -c q -c q -u bundles.vim && ln -s ~/.vim/vimrc ~/.vimrc
